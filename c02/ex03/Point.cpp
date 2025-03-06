7990b6f99943f0150af80e62d198196f410cd8ca
MOHAMED AIT TAJANTE <mhdjant607@gmail.com>
2025-03-06 22:55:14 +0000

    c02 before push1

--- c02/ex03/Point.cpp
+++ c02/ex03/Point.cpp
@@ -6,46 +6,37 @@
 /*   By: mait-taj <mait-taj@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 15:27:55 by mait-taj          #+#    #+#             */
-/*   Updated: 2025/02/15 12:06:36 by mait-taj         ###   ########.fr       */
+/*   Updated: 2025/03/06 22:54:56 by mait-taj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
-Point::Point() : x(0), y(0) { return ; }
+Point::Point() : x(0), y(0) { }
 
 Point::~Point() {}
 
-Point::Point( const float fl1, const float fl2 ) : x(fl1), y(fl2) { return ;}
+Point::Point( const float fl1, const float fl2 ) : x(fl1), y(fl2) { }
 
 Point::Point( const Point& other ) {
 	*this = other;
 }
 
 Point&	Point::operator=( const Point& obj ) {
-	if ( this != &obj ) {
-		this->x = obj.getFirstFixedPoint();
-		this->y = obj.getSecondFixedPoint();
-	}
+	(void)obj;
 	return *this;
 }
 
-const Fixed&	Point::getFirstFixedPoint( void ) const{
+const Fixed&	Point::getXX( void ) const {
 	return this->x;
 }
-const Fixed&	Point::getSecondFixedPoint( void ) const{
+
+const Fixed&	Point::getYY( void ) const {
 	return this->y;
 }
 
-void	Point::setFixedPoint( const Fixed& fix1, const Fixed& fix2 ) {
-	this->x = fix1;
-	this->y = fix2;
-}
-
 Fixed	Point::calculateArea( const Point& a, const Point& b, const Point& c ) {
-	Fixed	area = a.getFirstFixedPoint * ( b.getSecondFixedPoint - c.getSecondFixedPoint ) + b.getFirstFixedPoint * ( c.getSecondFixedPoint - a.getSecondFixedPoint ) + c.getFirstFixedPoint * ( a.getSecondFixedPoint - b.getSecondFixedPoint );
+	Fixed	area = a.getXX * ( b.getYY - c.getYY ) + b.getXX * ( c.getYY - a.getYY ) + c.getXX * ( a.getYY - b.getYY );
 	area /= 2;
-	if (area < 0)
-		return (area * -1);
 	return area;
 }
 
@@ -54,4 +45,8 @@ void	Point::printBolean( bool stat ) {
 		std::cout << "the point is inside the triangle." << std::endl;
 	else
 		std::cout << "the point is a vertex or on edge." << std::endl;
+}
+
+bool	bsp( Point const a, Point const b, Point const c, Point const point ) {
+	// calculate the area of abc and subtriangle here , because i can't use copy constructor or copy assignment operator here _________
 }
--- c02/ex03/Point.hpp
+++ c02/ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: mait-taj <mait-taj@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 13:11:37 by mait-taj          #+#    #+#             */
-/*   Updated: 2025/02/15 12:06:02 by mait-taj         ###   ########.fr       */
+/*   Updated: 2025/03/06 22:47:07 by mait-taj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,8 @@ class	Point {
 		Point&	operator=( const Point& obj );
 		Point( const Point& other );
 		Point( const float fl1, const float fl2 );
-		void			setFixedPoint( const Fixed& fix1, const Fixed& fix2 );
-		const Fixed&			getFirstFixedPoint( void ) const;
-		const Fixed&			getSecondFixedPoint( void ) const;
+		const Fixed&			getXX( void ) const;
+		const Fixed&			getYY( void ) const;
 		static Fixed	calculateArea( const Point& a, const Point& b, const Point& c );
 		static	void	printBolean( bool stat );
 };
