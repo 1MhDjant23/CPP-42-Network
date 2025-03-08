5970c11f25de34e8ac16063ef9ad904cabd883c6
MOHAMED AIT TAJANTE <mhdjant607@gmail.com>
2025-03-08 18:13:30 +0000

    c02 before push3

--- c02/ex01/main.cpp
+++ c02/ex01/main.cpp
@@ -6,26 +6,26 @@
 /*   By: mait-taj <mait-taj@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 11:42:28 by mait-taj          #+#    #+#             */
-/*   Updated: 2025/03/06 17:27:56 by mait-taj         ###   ########.fr       */
+/*   Updated: 2025/03/08 18:12:12 by mait-taj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int main( void ) {
-	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
+	Fixed a(1.6f);
+	// Fixed const b( 10 );
+	// Fixed const c( 42.42f );
+	// Fixed const d( b );
 
-	a = Fixed( 1234.4321f );
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	// a = Fixed( 1234.4321f );
+	// std::cout << "a is " << a << std::endl;
+	// std::cout << "b is " << b << std::endl;
+	// std::cout << "c is " << c << std::endl;
+	// std::cout << "d is " << d << std::endl;
+	// std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	// std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	// std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	// std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return 0;
 }
